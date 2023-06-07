42c96050032d124fc044bff18e6fa031df4e5a2c
PeterJames <davoine.pierre@hotmail.fr>
2023-06-07 13:20:29 +0200

    Add the get all quiz route. Update of handle errors

--- app/controllers/quiz.controller.js
+++ app/controllers/quiz.controller.js
@@ -1,24 +1,45 @@
 const { Quiz } = require('../models');
 
 const controller = {
-  //* Récupérer un quizz par rapport à son id
-  getOneQuiz: async (req, res) => {
-    const { quizId } = req.params;
-    console.log(quizId);
+  //* Récupérer la liste des quiz
+  getAllQuiz: async (req, res) => {
+    console.log('getAllQuiz');
     try {
-      // On va chercher le quizz en BDD
-      // const quiz = await datamapper.getOneQuiz(quizId);
-      const quiz = await Quiz.findByPk(quizId);
+      // On va chercher la liste des quiz en BDD
+      const quiz = await Quiz.findAll();
+      // Si il n'y a pas de quiz, on renvoie une erreur
       if (!quiz) {
-        throw new Error(
-          "Une erreur est survenue lors de la recherche du quiz ou le quiz n'héxiste pas."
-        );
+        console.log('Aucun résultat trouvé.');
+        return res.status(404).json({ message: 'Aucun quiz trouvé.' });
       }
+      // Sinon on renvoie la liste des quiz
       return res.json(quiz);
     } catch (err) {
-      //Gestion de l'erreur
       console.error(err);
-      return res.status(204).send(err);
+      return res
+        .status(500)
+        .json({ error: 'Le serveur à rencontré un problème.' });
+    }
+  },
+
+  //* Récupérer un quizz intégralement via à son id
+  getOneQuiz: async (req, res) => {
+    const { quizId } = req.params;
+    try {
+      // On va chercher le quizz en BDD
+      const quiz = await Quiz.findByPk(quizId);
+      // Si le quizz n'existe pas, on renvoie une erreur
+      if (!quiz) {
+        console.log('Aucun résultat trouvé.');
+        return res.status(404).json({ message: "Aucun quiz n'a été trouvé." });
+      }
+      // Sinon on renvoie le quizz
+      return res.json(quiz);
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: 'Le serveur à rencontré un problème.' });
     }
   },
 };
